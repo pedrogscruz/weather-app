7bad08a5ec2999775a1f7e5f164b827e07c5ac91
pedrogscruz <pedro.gscruz@hotmail.com>
2025-03-27 02:12:09 -0300

    Create basic home page structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchTerm = ref('')
+</script>
+
+<template>
+  <div class="weather-container">
+    <h1 class="title">Weather</h1>
+    <input v-model="searchTerm" type="text" placeholder="Search city" class="search-bar" />
+  </div>
+</template>
+
+<style scoped>
+.weather-container {
+  max-width: 600px;
+  margin: auto;
+  padding: 20px;
+}
+
+.title {
+  font-size: 2rem;
+  margin-bottom: 10px;
+}
+
+.search-bar {
+  width: -webkit-fill-available;
+  padding: 8px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
